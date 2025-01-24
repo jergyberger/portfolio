7895c11ff63ff39c9305215678519c80b2f7aae6
Jørgen Maurstad Leiros <joma23ak@student.cbs.dk>
2025-01-24 18:19:37 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0px";
-    document.querySelector(".main-content").style.marginLeft = "0px";
-}
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "400px";
-    document.querySelector(".main-content").style.marginLeft = "400px";
-}
